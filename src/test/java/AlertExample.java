efa23d4fd1cec99516d29623827e48658fe83b7d
HarithaGane99 <haritha.99ganegoda@gmail.com>
2026-01-22 17:00:45 +0530

    alert box done

--- src/test/java/AlertExample.java
+++ src/test/java/AlertExample.java
@@ -16,13 +16,33 @@ public class AlertExample {
     }
 
     @Test
-    public void alertTest(){
+    public void alertTest() throws InterruptedException {
         driver.get("https://www.leafground.com/alert.xhtml");
 
         //01 Alert (simple dialog)
         driver.findElement(By.id("j_idt88:j_idt91")).click();
         Alert alert =driver.switchTo().alert();
+        Thread.sleep(2000);
         alert.accept();
+
+        //02 Cancel Button (confirm dialog)
+        driver.findElement(By.id("j_idt88:j_idt93")).click();
+
+        Alert alert2 =driver.switchTo().alert();
+        Thread.sleep(2000);
+        alert2.dismiss();
+
+
+        //03 Typing Alert Box
+        driver.findElement(By.id("j_idt88:j_idt104")).click();
+        Alert alert3 =driver.switchTo().alert();
+        Thread.sleep(2000);
+        String alertText = alert3.getText();
+        System.out.println("Alert Text: " + alertText);
+        alert3.sendKeys("Haritha");
+        alert.accept();
+
+
 
     }
 }
